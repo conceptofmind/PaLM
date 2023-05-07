a159ddec8715322180a9fd509c54c4f214a1b189
Enrico Shippole <enricoship@gmail.com>
2023-05-07 19:35:47 -0400

    Update

--- hubconf.py
+++ hubconf.py
@@ -18,3 +18,39 @@ def palm_150m_8k_v0():
     model.load_state_dict(state_dict)
 
     return model
+
+def palm_410m_8k_v0():
+    num_tokens = 50304
+    dim = 1024
+    depth = 24
+    dim_head = 128
+    heads = 8
+    flash_attn = True
+
+    model = PaLM(
+        num_tokens=num_tokens, dim=dim, depth=depth, dim_head=dim_head, heads=heads, flash_attn=flash_attn
+    )
+
+    hf_url = 'https://huggingface.co/conceptofmind/palm-150m/resolve/main/palm_410m_8k_v0.pt'
+    state_dict = torch.hub.load_state_dict_from_url(hf_url)
+    model.load_state_dict(state_dict)
+
+    return model
+
+def palm_1b_8k_v0():
+    num_tokens = 50304
+    dim = 2048
+    depth = 16
+    dim_head = 128
+    heads = 8
+    flash_attn = True
+
+    model = PaLM(
+        num_tokens=num_tokens, dim=dim, depth=depth, dim_head=dim_head, heads=heads, flash_attn=flash_attn
+    )
+
+    hf_url = 'https://huggingface.co/conceptofmind/palm-150m/resolve/main/palm_1b_8k_v0.pt'
+    state_dict = torch.hub.load_state_dict_from_url(hf_url)
+    model.load_state_dict(state_dict)
+
+    return model
