61e4a39644db73d91aaf8a4cb72838624493f0b5
Enrico Shippole <enricoship@gmail.com>
2023-05-30 13:29:57 -0400

    Add folder and utils

---
+++ palm/utils.py
@@ -0,0 +1,10 @@
+import torch.distributed as dist  # Add this line
+
+def print_num_params(model):
+    n_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+
+    if dist.is_available():
+        if dist.get_rank() == 0:
+            print(f"Number of parameters in model: {n_params}")
+    else:
+        print(f"Number of parameters in model: {n_params}")
